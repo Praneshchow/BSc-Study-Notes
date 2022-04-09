cdcd176050c6e3266fdca03f7735f65c8958c1c3
Pranesh Chowdhruy <80632860+Pranesh98@users.noreply.github.com>
2022-04-09 22:42:46 +0600

    Create Palindrome number (Function) .c

---
+++ C code/Palindrome number (Function) .c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int num, temp, sum = 0;
+    scanf("%d", &num);
+    
+    palindrome (temp, num, sum);
+    checker(sum, num);
+
+    return 0;
+}
+int palindrome (int temp, int num, int sum){
+    int r;
+    temp = num;
+    while (temp != 0){
+        r = temp % 10;
+        return sum = sum * 10 + r;
+        temp = temp / 10;
+    }
+}
+int checker (int sum, int num){
+    if (num == sum ){
+        printf("It is Palindrome Number");
+    }
+    else {
+        printf("It's not a Palindrome Number");
+    }
+}
